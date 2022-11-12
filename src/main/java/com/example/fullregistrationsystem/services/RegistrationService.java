0528ab3204c4df5a1d61d0c7d31e725beba7a094
Adam Nguyen Van <104433146+NoxEtSol@users.noreply.github.com>
2022-11-12 13:34:28 +0100

    Feat: Security configuration implemented

---
+++ src/main/java/com/example/fullregistrationsystem/services/RegistrationService.java
@@ -0,0 +1,10 @@
+package com.example.fullregistrationsystem.services;
+
+import com.example.fullregistrationsystem.config.RegistrationRequest;
+
+public class RegistrationService {
+
+  public String register(RegistrationRequest request) {
+    return "works";
+  }
+}
